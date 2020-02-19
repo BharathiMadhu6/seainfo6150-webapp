26c66f59913c350a042cd1c3220e1cfc8b73eda7
BharathiMadhu6 <bharathimadhu@bharathis-MacBook-Pro.local>
2020-02-19 00:55:19 -0800

    assignment 6

---
+++ src/ArticleList/ArticleImage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./ArticleImage.module.css";
+
+const ArticleImage = props => {
+    return (
+        <div> 
+            
+                 <img className = {styles.image} src = {props.url._url} alt ="image" /> 
+                 <h3 className = {styles.title}> {props.title} </h3>
+            
+        </div>    
+    );
+};
+
+ArticleImage.propTypes = {
+    url : PropTypes.string.isRequired,
+    title : PropTypes.string.isRequired
+};
+
+export default ArticleImage;
--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./ArticleListItem.module.css";
 import SlugButton from "./SlugButton";
+import ArticleImage from "./ArticleImage";
 
 const ArticleListItem = props => {
   return (
     <div className={styles.container}>
-      <h3 className={styles.title}>{props.article.title}</h3>
+      <ArticleImage url = {props.article.image} title = {props.article.title} />
       <p>{props.article.shortText}</p>
       <time className={styles.pubDate} dateTime={props.article.pubYear}>{props.article.pubDate}</time>
       <SlugButton slug={props.article.slug} buttonText={props.article.author} />
